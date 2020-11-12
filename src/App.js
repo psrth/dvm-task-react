f89a971a08f8f1451f22e954e1da9c96e76d8ea3
Parth Sharma <parthsharma151@gmail.com>
2020-11-12 16:58:13 +0530

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/button'
+import Header from './components/Header'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header text="Auth Routing Test"/>
+        <Button variant="light" className="navB">Login</Button>        
+        <Button variant="light" className="navB">Signup</Button>
+        <Button variant="light" className="navB">Home</Button>
+        <Button variant="light" className="navB">About</Button>
       </header>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../App.css';
+
+function Header(props){
+    return(
+        <h1>{ props.text }</h1>
+    )
+}
+
+export default Header;
